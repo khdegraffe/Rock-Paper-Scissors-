16053135e6aab15147c6748ff3fc5ed78adb13e1
khdegraffe <khdegraffe@gmail.com>
2025-05-17 20:35:38 -0400

    add grades exercise

--- conditionalPractice.js
+++ conditionalPractice.js
@@ -1,5 +1,14 @@
-let message = 4;
-
-if (message >= 4) {
-  alert("Hello, World!");
+let testGrade = Math.floor(Math.random() * 100);
+console.log(testGrade);
+let studentName = "Poppy";
+if (testGrade <= 64) {
+  console.log(`${studentName} received a F.`);
+} else if (testGrade <= 70) {
+  console.log(`${studentName} recieved a D.`);
+} else if (testGrade <= 79) {
+  console.log(`${studentName} recieved a C.`);
+} else if (testGrade <= 89) {
+  console.log(`${studentName} recieved a B.`);
+} else {
+  console.log(`${studentName} received an A!!`);
 }
